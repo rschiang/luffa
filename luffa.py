02807e16207170c4dd704e546eb82cfc89d6e108
Poren Chiang <ren.chiang@gmail.com>
2015-06-06 03:38:04 +0800

    Simple implementation of Luffa

--- luffa.py
+++ luffa.py
@@ -1,9 +1,37 @@
-from bottle import route
+import json
+from bottle import get, post, request, HTTPError
+from urllib.request import Request, urlopen
 
-@route('/')
+def get_settings():
+    with open('config.json') as f:
+        return json.load(f)
+
+@get('/')
 def index():
     return 'It works!'
 
-@route('/broadcast')
-def broadcast():
-    return 'All is well'
+@post('/broadcast/<team>')
+def broadcast(team):
+    settings = get_settings()
+    if team not in settings or request.forms.get('token') != settings[team]['token']:
+        return HTTPError(status=401)
+
+    # Build up message payload
+    message = {
+        "username": "{} ({})".format(request.forms.get('user_name'), settings[team]['slug']),
+        "text": request.forms.get('text'),
+    }
+
+    for site, info in settings.items():
+        if site == team:
+            continue    # Skip ourselves
+
+        query = Request(info['publish_hook'], data=json.dumps(message))
+        query.add_header('Content-Type', 'application/json')
+
+        try:
+            urlopen(query)
+        except:
+            return HTTPError(status=502)
+
+    return ''
