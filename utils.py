b6d9c5488998e208693264b32a3fd1b0f9866d3e
Poren Chiang <ren.chiang@gmail.com>
2015-06-06 17:34:03 +0800

    Fix incorrect Slack API usage

--- utils.py
+++ utils.py
@@ -11,19 +11,19 @@ def get_slacker(team):
 
 def get_channel(team, identifier):
     slack = get_slacker(team)
-    response = slack.channels.info(identifier)
+    response = slack.channels.info(identifier).body
     return response['channel']['name'] if response.get('ok') else None
 
 def get_user(team, identifier):
     slack = get_slacker(team)
-    response = slack.users.info(identifier)
+    response = slack.users.info(identifier).body
     return response['user'] if response.get('ok') else None
 
 def translate_user(team1, team2, identifier):
     user1 = get_user(team1, identifier)
     if user1:
         slack2 = get_slacker(team2)
-        users2 = { item['name']: item for item in slack2.users.list()['members'] }
+        users2 = { item['name']: item for item in slack2.users.list().body['members'] }
         user2 = users2.get(user1['name'])
         if user2:
             return '<@{}|{}>'.format(user2['id'], user1['name'])
