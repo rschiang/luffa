c4361421532ddb5310c5db0ad1ad1cfefdcd2f2d
Poren Chiang <ren.chiang@gmail.com>
2015-06-06 14:29:23 +0800

    Dirty implementation of user info fetching

--- luffa.py
+++ luffa.py
@@ -2,6 +2,7 @@ import json
 import re
 import traceback
 from bottle import default_app, get, post, request, HTTPError
+from slack import Slacker
 from urllib.request import Request, urlopen
 
 def get_settings():
@@ -26,9 +27,23 @@ def broadcast(team):
     # Build up message payload
     text = re.sub(r'\<([@#])([^\|]+)\|([^\>]+)\>', r'\1\3', data.get('text'))
     message = {
-        "username": "{} ({})".format(data.get('user_name'), settings[team]['slug']),
+        "username": data.get('user_name'),
         "text": text,
     }
+
+    if 'query_token' in settings[team]:
+        slack = Slacker(settings[team]['query_token'])
+        try:
+            message['icon_url'] = slack.user.info(data.get('user_id'))['user']['profile']['image_192']
+        except:
+            pass
+
+        def find_user(match):
+            try:
+                return slack.user.info(match.group(1))['user']['name']
+            except:
+                return match.group(0)
+        text = re.sub(r'\<@([^\>]+)\>', find_user, text)
 
     for site, info in settings.items():
         if site == team:
